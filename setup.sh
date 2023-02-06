79e91e86b21b050e1511233df925956164ca6add
roctbb <roctbb@gmail.com>
2023-02-06 21:30:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo cp agents_dummy.conf /etc/supervisor/conf.d/
 sudo cp agents_dummy_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
-sudo certbot --nginx -d dummy.medsenger.ru
+sudo certbot --nginx -d dummy.ai.medsenger.ru
