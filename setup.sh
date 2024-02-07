79c01512d878d086f60a3fe7df25160404e9f564
roctbb <roctbb@gmail.com>
2024-02-07 14:46:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pip3 install -r requirements.txt
+sudo cp dummy.ini /etc/uwsgi/apps/
 sudo cp agents_dummy.conf /etc/supervisor/conf.d/
 sudo cp agents_dummy_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
